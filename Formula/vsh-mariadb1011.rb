e497eb86c26f8e3806f2094e6eb5f7c42719e2bb
Philipp Dittert <p.dittert@techdivision.com>
2025-06-27 12:49:45 +0200

    add mariadb 10.11

---
+++ Formula/vsh-mariadb1011.rb
@@ -0,0 +1,226 @@
+class VshMariadb1011 < Formula
+  desc "Drop-in replacement for MySQL"
+  homepage "https://mariadb.org/"
+  url "https://archive.mariadb.org/mariadb-10.11.13/source/mariadb-10.11.13.tar.gz"
+  sha256 "f8b734749fbd652ea4e255be8cc7880f98d07b6a7feb4e1ea8c736cb480d23e4"
+  license "GPL-2.0-only"
+  revision 1
+
+  bottle do
+    root_url "https://github.com/valet-sh/homebrew-core/releases/download/bottles"
+    sha256 ventura: "0a293214d906e7077be80f661b58ceef91e955eb8939fff02d580156d2dfef94"
+  end
+
+  depends_on "bison" => :build
+  depends_on "cmake" => :build
+  depends_on "fmt" => :build
+  depends_on "openjdk" => :build
+  depends_on "pkgconf" => :build
+
+  depends_on "groonga"
+  depends_on "lz4"
+  depends_on "lzo"
+  depends_on "openssl@3"
+  depends_on "pcre2"
+  depends_on "xz"
+  depends_on "zstd"
+
+  uses_from_macos "bzip2"
+  uses_from_macos "krb5"
+  uses_from_macos "libedit"
+  uses_from_macos "libxcrypt"
+  uses_from_macos "libxml2"
+  uses_from_macos "ncurses"
+  uses_from_macos "zlib"
+
+  def datadir
+    var/"#{name}"
+  end
+
+  def tmpconfdir
+    libexec/"config"
+  end
+
+  def install
+    ENV.runtime_cpu_detection
+    ENV.cxx11
+
+    # Backport fix for CMake 4.0 in columnstore submodule
+    # https://github.com/mariadb-corporation/mariadb-columnstore-engine/commit/726cc3684b4de08934c2b14f347799fd8c3aac9a
+    # https://github.com/mariadb-corporation/mariadb-columnstore-engine/commit/7e17d8825409fb8cc0629bfd052ffac6e542b50e
+    inreplace "storage/columnstore/columnstore/CMakeLists.txt",
+              "CMAKE_MINIMUM_REQUIRED(VERSION 2.8.12)",
+              "CMAKE_MINIMUM_REQUIRED(VERSION 3.10)"
+
+    # Set basedir and ldata so that mysql_install_db can find the server
+    # without needing an explicit path to be set. This can still
+    # be overridden by calling --basedir= when calling.
+    inreplace "scripts/mysql_install_db.sh" do |s|
+      s.change_make_var! "basedir", "\"#{opt_libexec}\""
+      s.change_make_var! "ldata", "\"#{datadir}\""
+    end
+
+    # Use brew groonga
+    rm_r "storage/mroonga/vendor/groonga"
+
+    # -DINSTALL_* are relative to prefix
+    args = %W[
+      -DCMAKE_INSTALL_PREFIX=#{libexec}
+      -DMYSQL_DATADIR=#{datadir}
+      -DINSTALL_INCLUDEDIR=include/mysql
+      -DINSTALL_MANDIR=share/man
+      -DINSTALL_DOCDIR=share/doc/#{name}
+      -DINSTALL_INFODIR=share/info
+      -DINSTALL_MYSQLSHAREDIR=share/mysql
+      -DWITH_LIBFMT=system
+      -DWITH_SSL=system
+      -DWITH_UNIT_TESTS=OFF
+      -DDEFAULT_CHARSET=utf8mb4
+      -DDEFAULT_COLLATION=utf8mb4_general_ci
+      -DINSTALL_SYSCONFDIR=#{etc}/#{name}
+      -DCOMPILATION_COMMENT=valet.sh
+      -DPLUGIN_ROCKSDB=NO
+    ]
+
+    system "cmake", "-S", ".", "-B", "_build", *std_cmake_args, *args
+    system "cmake", "--build", "_build"
+    system "cmake", "--install", "_build"
+
+    # Save space
+    #rm_r libexec/"mariadb-test"
+    #rm_r libexec/"sql-bench"
+
+    # Don't create databases inside of the prefix!
+    # See: https://github.com/Homebrew/homebrew/issues/4975
+    rm_rf prefix/"data"
+
+    # Link the setup script into bin
+    (libexec/"bin").install_symlink libexec/"scripts/mysql_install_db"
+
+    # Save space
+    #(prefix/"mysql-test").rmtree
+    #(prefix/"sql-bench").rmtree
+
+    # Link the setup script into bin
+    #bin.install_symlink prefix/"scripts/mysql_install_db"
+
+    # Fix up the control script and link into bin
+    #inreplace "#{prefix}/support-files/mysql.server", /^(PATH=".*)(")/, "\\1:#{HOMEBREW_PREFIX}/bin\\2"
+
+    #bin.install_symlink prefix/"support-files/mysql.server"
+
+    #libexec.install "bin", "docs", "include", "lib", "man", "share"
+
+    (bin/"mariadb10.11").write <<~EOS
+      #!/bin/bash
+      #{libexec}/bin/mysql --defaults-file=#{etc}/#{name}/my.cnf "$@"
+    EOS
+    (bin/"mariadump10.11").write <<~EOS
+      #!/bin/bash
+      #{libexec}/bin/mysqldump --defaults-file=#{etc}/#{name}/my.cnf "$@"
+    EOS
+    (bin/"mariaadmin10.11").write <<~EOS
+      #!/bin/bash
+      #{libexec}/bin/mysqladmin --defaults-file=#{etc}/#{name}/my.cnf "$@"
+    EOS
+
+    tmpconfdir.mkpath
+    (tmpconfdir/"my.cnf").write <<~EOS
+        !includedir #{etc}/#{name}/conf.d/
+    EOS
+    (tmpconfdir/"mysqld.cnf").write <<~EOS
+        [mysqld_safe]
+        socket =
+        nice		= 0
+
+        [mysqld]
+        socket =
+
+        bind-address = 127.0.0.1
+        port		= 3323
+        basedir		= #{opt_libexec}
+        datadir		= #{datadir}
+        tmpdir		= /tmp
+        lc-messages-dir	= #{opt_libexec}/share
+        skip-external-locking
+
+        key_buffer_size		= 16M
+        max_allowed_packet	= 16M
+        thread_stack		= 192K
+        thread_cache_size       = 8
+        myisam-recover-options  = BACKUP
+        max_connections        = 200
+        log_error = #{var}/log/#{name}/error.log
+        max_binlog_size   = 100M
+
+        character-set-server=utf8mb4
+        collation-server=utf8mb4_general_ci
+    EOS
+    (tmpconfdir/"mysqldump.cnf").write <<~EOS
+        [mysqldump]
+        user = root
+        protocol=tcp
+        port=3323
+        host=127.0.0.1
+        quick
+        quote-names
+        max_allowed_packet	= 128M
+        default-character-set = utf8mb4
+    EOS
+
+    # move config files into etc
+    rm_rf etc/"#{name}/my.cnf"
+    (etc/"#{name}").install tmpconfdir/"my.cnf"
+    (etc/"#{name}/conf.d").install tmpconfdir/"mysqld.cnf"
+    (etc/"#{name}/conf.d").install tmpconfdir/"mysqldump.cnf"
+
+  end
+
+  def post_install
+    (var/"log/#{name}").mkpath
+    # make sure the datadir exists
+    datadir.mkpath
+
+        # Don't initialize database, it clashes when testing other MySQL-like implementations.
+    return if ENV["HOMEBREW_GITHUB_ACTIONS"]
+
+    unless File.exist? "#{datadir}/mysql/user.frm"
+      ENV["TMPDIR"] = nil
+      system libexec/"bin/mysql_install_db", "--verbose", "--auth-root-authentication-method=normal", "--user=#{ENV["USER"]}",
+        "--basedir=#{libexec}", "--datadir=#{datadir}", "--tmpdir=/tmp"
+    end
+  end
+
+  def caveats
+    s = <<~EOS
+      MariaDB 10.11 is configured to only allow connections from 127.0.0.1 on port 3323 by default
+
+      To connect run:
+          mariadb10.11 -uroot
+    EOS
+    s
+  end
+
+  service do 
+    run [libexec/"bin/mysqld_safe", "--defaults-file=#{etc}/vsh-mariadb1011/my.cnf", "--datadir=#{var}/vsh-mariadb1011"]
+    keep_alive true
+    working_dir var/"vsh-mariadb1011"
+  end
+
+  test do
+    (testpath/"mysql").mkpath
+    (testpath/"tmp").mkpath
+    system bin/"mysql_install_db", "--no-defaults", "--user=#{ENV["USER"]}",
+      "--basedir=#{prefix}", "--datadir=#{testpath}/mysql", "--tmpdir=#{testpath}/tmp",
+      "--auth-root-authentication-method=normal"
+    port = free_port
+    fork do
+      system bin/"mysqld", "--no-defaults", "--user=#{ENV["USER"]}",
+        "--datadir=#{testpath}/mysql", "--port=#{port}", "--tmpdir=#{testpath}/tmp"
+    end
+    sleep 5
+    assert_match "information_schema",
+      shell_output("#{bin}/mysql --port=#{port} --user=root --password= --execute='show databases;'")
+    system bin/"mysqladmin", "--port=#{port}", "--user=root", "--password=", "shutdown"
+  end
+end
