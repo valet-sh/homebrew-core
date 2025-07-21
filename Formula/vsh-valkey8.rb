efd060f2372a6d3f7b452a55b3169c6f361470c3
Philipp Dittert <p.dittert@techdivision.com>
2025-07-21 10:47:25 +0200

    update valkey formula

--- Formula/vsh-valkey8.rb
+++ Formula/vsh-valkey8.rb
@@ -3,7 +3,7 @@ class VshValkey8 < Formula
   homepage "https://valkey.io"
   url "https://github.com/valkey-io/valkey/archive/refs/tags/8.1.2.tar.gz"
   sha256 "747b272191c15c7387f4ad3b3e7eda16deb1cffc6425e0571547f54e4d2e3646"
-  revision 4
+  revision 5
   license all_of: [
     "BSD-3-Clause",
     "BSD-2-Clause", # deps/jemalloc, deps/linenoise, src/lzf*
@@ -39,7 +39,7 @@ class VshValkey8 < Formula
 
     # Fix up default conf file to match our paths
     inreplace "valkey.conf" do |s|
-      s.gsub! "/var/run/valkey_6379.pid", vardir/"run/valkey.pid"
+      s.gsub! "/var/run/valkey_6379.pid", vardir/"run/valkey8.pid"
       s.gsub! "dir ./", "dir #{vardir}/db/valkey/"
       s.gsub! "port 6379", "port 6389"
       s.sub!(/^bind .*$/, "bind 127.0.0.1 ::1")
