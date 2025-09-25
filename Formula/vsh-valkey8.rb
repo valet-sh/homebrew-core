7b46a50042e469661a96d412a5de9151dc16f3cd
Philipp Dittert <p.dittert@techdivision.com>
2025-09-25 22:16:46 +0200

    bump revision for valkey8

--- Formula/vsh-valkey8.rb
+++ Formula/vsh-valkey8.rb
@@ -3,7 +3,7 @@ class VshValkey8 < Formula
   homepage "https://valkey.io"
   url "https://github.com/valkey-io/valkey/archive/refs/tags/8.1.2.tar.gz"
   sha256 "747b272191c15c7387f4ad3b3e7eda16deb1cffc6425e0571547f54e4d2e3646"
-  revision 6
+  revision 7
   license all_of: [
     "BSD-3-Clause",
     "BSD-2-Clause", # deps/jemalloc, deps/linenoise, src/lzf*
@@ -14,7 +14,7 @@ class VshValkey8 < Formula
 
   bottle do
     root_url "https://github.com/valet-sh/homebrew-core/releases/download/bottles"
-    sha256 ventura: "c6ed56755652385c0404a437e765228ffbe10620ed510f5697ee548008797f11"
+    sha256 sonoma: "c6ed56755652385c0404a437e765228ffbe10620ed510f5697ee548008797f11"
   end
 
   depends_on "openssl@3"
