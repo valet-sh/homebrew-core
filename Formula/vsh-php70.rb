47db156577842b22da7c52423cf50504ab706201
zelgerj <zelgerj@users.noreply.github.com>
2025-09-26 02:10:19 +0000

    Trigger bottle rebuild

--- Formula/vsh-php70.rb
+++ Formula/vsh-php70.rb
@@ -5,7 +5,7 @@ class VshPhp70 < Formula
   version "7.0.33"
   sha256 "2d80d4186c14aa7e75cca38105359eda808a512a57824462e84e96d5b1be6b5c"
   license "PHP-3.01"
-  revision 565
+  revision 566
 
   bottle do
     root_url "https://github.com/valet-sh/homebrew-core/releases/download/bottles"
