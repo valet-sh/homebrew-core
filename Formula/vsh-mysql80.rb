7ab02b1ac40a7e2e740183ff811aef9c6462bf12
Philipp Dittert <p.dittert@techdivision.com>
2026-06-24 08:28:07 +0200

    bump revision

--- Formula/vsh-mysql80.rb
+++ Formula/vsh-mysql80.rb
@@ -4,7 +4,7 @@ class VshMysql80 < Formula
   url "https://cdn.mysql.com/Downloads/MySQL-8.0/mysql-boost-8.0.46.tar.gz"
   sha256 "dff4332ee7f8f37fc0516c66763600a22a81c8192c743c477b6484206e314f2f"
   license "GPL-2.0-only" => { with: "Universal-FOSS-exception-1.0" }
-  revision 1
+  revision 2
 
   bottle do
     root_url "https://github.com/valet-sh/homebrew-core/releases/download/bottles"
