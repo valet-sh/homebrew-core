55d2a90596d2faeb686bd47266e343edb3b7c8d6
CI <ci@valet.sh>
2026-06-24 07:35:09 +0000

    [skip ci] Update bottle information in formulae ***NO_CI***

--- Formula/vsh-mysql84.rb
+++ Formula/vsh-mysql84.rb
@@ -8,7 +8,7 @@ class VshMysql84 < Formula
 
   bottle do
     root_url "https://github.com/valet-sh/homebrew-core/releases/download/bottles"
-    sha256 sonoma: "dc84ffed94f39c4b32609ce2623484b5c071447fe5a86702b3a74fef641f6798"
+    sha256 sonoma: "b09a52a35962bc128cdd635bed0626da9b75e00675d9588cf20033e160124160"
   end
 
   depends_on "bison" => :build
